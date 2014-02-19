570bb2d2130b9c6a4722f2b996d3db54c17b1670
yetanothervan <yetanothervan@gmail.com>
2014-02-19 16:50:13 +0400

    + layout camomile

--- Services/GraphOrganizeService/LayoutCamomile.cs
+++ Services/GraphOrganizeService/LayoutCamomile.cs
@@ -43,7 +43,8 @@ namespace GraphOrganizeService
                             YeildChapterLayoutRow(pagesSet,
                                 ExtractRowForBlockInChapter(pagesSet, chapter.ChapterBlock.BlockId, page, null)));
                     }
-                    
+
+                    rows.ForEach(r => r.MyChapter = chapter);
                     rows.ForEach(ApplyLayout);
                     
                     var layout = new ChapterLayout {Rows = rows, ChapterBlock = chapter.ChapterBlock};
@@ -54,30 +55,23 @@ namespace GraphOrganizeService
             return grid;
         }
 
-        private void ApplyLayout(ChapterLayoutRow chapterLayoutRow)
+        private void ApplyLayout(ChapterLayoutRow row)
         {
-            if (chapterLayoutRow.Pages == null || chapterLayoutRow.Pages.Count == 0) return;
+            if (row.Pages == null || row.Pages.Count == 0) return;
 
-            if (chapterLayoutRow.Pages.Count == 1)
+            if (row.Pages.Count == 1)
             {
-                chapterLayoutRow.ThirdColumn.Add(
-                    new ChapterLayoutElem {Page = chapterLayoutRow.Pages[0], RowSpan = 1});
+                row.ThirdColumn.Add(
+                    new ChapterLayoutElem {Page = row.Pages[0], RowSpan = 1});
+                if (row.Pages[0].MySources == BlockQuoteParticleSources.MyBook)
+                    row.Inner = true;
                 return;
             }
 
-            if (chapterLayoutRow.Pages.Count == 3)
+            if (row.Pages.Count == 3)
             {
-                var rel = chapterLayoutRow.Pages.FirstOrDefault(r => r.IsBlockRel);
-                if (rel != null)
-                {
-                    chapterLayoutRow.ThirdColumn.Add(
-                        new ChapterLayoutElem {Page = rel, RowSpan = 1});
-                    chapterLayoutRow.SecondColumn.Add(
-                        new ChapterLayoutElem {Page = rel.RelationFirst, RowSpan = 1});
-                    chapterLayoutRow.FourthColumn.Add(
-                        new ChapterLayoutElem { Page = rel.RelationSecond, RowSpan = 1 });
-                }
-                return;
+                var trip = new ChapterLayoutRowTriplet(row);
+                trip.DoLayout();
             }
         }
 
@@ -121,25 +115,30 @@ namespace GraphOrganizeService
             selem.PlaceOn(0, chapCount * 4 + 2);
 
             int whole = 0;
+            int wholeUnder = 1;
             foreach (var chapterLayoutRow in layout.Rows)
             {
+                int delta = chapterLayoutRow.Inner ? wholeUnder : whole;
                 int c = 0;
                 foreach (var elem in chapterLayoutRow.FirstColumn)
-                    PlaceElemInGrid(elem, grid, c -= elem.RowSpan - whole, chapCount*4);
+                    PlaceElemInGrid(elem, grid, c -= elem.RowSpan - delta, chapCount*4);
 
                 c = 0;
                 foreach (var elem in chapterLayoutRow.SecondColumn)
-                    PlaceElemInGrid(elem, grid, c -= elem.RowSpan - whole, chapCount*4 + 1);
+                    PlaceElemInGrid(elem, grid, c -= elem.RowSpan - delta, chapCount*4 + 1);
 
                 c = 0;
                 foreach (var elem in chapterLayoutRow.ThirdColumn)
-                    PlaceElemInGrid(elem, grid, c -= elem.RowSpan - whole, chapCount*4 + 2);
+                    PlaceElemInGrid(elem, grid, c -= elem.RowSpan - delta, chapCount*4 + 2);
 
                 c = 0;
                 foreach (var elem in chapterLayoutRow.FourthColumn)
-                    PlaceElemInGrid(elem, grid, c -= elem.RowSpan - whole, chapCount*4 + 3);
+                    PlaceElemInGrid(elem, grid, c -= elem.RowSpan - delta, chapCount*4 + 3);
 
-                whole -= chapterLayoutRow.Height;
+                if (chapterLayoutRow.Inner)
+                    wholeUnder += chapterLayoutRow.Height;
+                else
+                    whole -= chapterLayoutRow.Height;
             }
         }
 
@@ -155,8 +154,6 @@ namespace GraphOrganizeService
             elem.PlaceOn(row, col);
         }
     }
-
-
 
 
     public class ChapterLayout
@@ -202,5 +199,113 @@ namespace GraphOrganizeService
 
         public List<IPage> Pages;
         public bool Inner;
+        public IChapter MyChapter;
+    }
+
+    public class ChapterLayoutRowTriplet
+    {
+        private readonly ChapterLayoutRow _row;
+        public ChapterLayoutRowTriplet(ChapterLayoutRow row)
+        {
+            _row = row;
+        }
+
+        public IPage Rel
+        {
+            get { return _row.Pages.First(r => r.IsBlockRel); }
+        }
+
+        public IPage BlockInChapter
+        {
+            get
+            {
+                return _row.Pages
+                    .FirstOrDefault(b => !b.IsBlockRel
+                                         && b.MyChapter != null
+                                         && b.MyChapter.ChapterBlock.BlockId == _row.MyChapter.ChapterBlock.BlockId);
+            }
+        }
+
+        public IPage Other(IPage block)
+        {
+            if (block == null) return null;
+            return _row.Pages
+                .FirstOrDefault(b => !b.IsBlockRel
+                                     && b.Block.BlockId != block.Block.BlockId);
+        }
+
+        public bool IsInChapter(IPage block)
+        {
+            if (block == null || block.MyChapter == null ||
+                block.MyChapter != _row.MyChapter) return false;
+            return true;
+        }
+
+        private bool IsLeftNeightbor(IPage block)
+        {
+            if (block != null && block.MyChapter != null
+                && block.MyChapter.NextChapter == _row.MyChapter) return true;
+            return false;
+        }
+
+        private bool IsRightNeightbor(IPage block)
+        {
+            if (block != null && block.MyChapter != null 
+                && block.MyChapter.PrevChapter == _row.MyChapter) return true;
+            return false;
+        }
+
+        public bool IsInBook(IPage block)
+        {
+            if (block != null && block.MyChapter != null 
+                && block.MyChapter.MyBook == _row.MyChapter.MyBook) return true;
+            return false;
+        }
+
+        public void DoLayout()
+        {
+            if (BlockInChapter != null)
+            {
+                var other = Other(BlockInChapter);
+                if (IsInChapter(other))
+                {
+                    _row.SecondColumn.Add(new ChapterLayoutElem {Page = BlockInChapter, RowSpan = 1});
+                    _row.ThirdColumn.Add(new ChapterLayoutElem {Page = Rel, RowSpan = 1});
+                    _row.FourthColumn.Add(new ChapterLayoutElem {Page = other, RowSpan = 1});
+                    return;
+                }
+
+                _row.ThirdColumn.Add(new ChapterLayoutElem {Page = BlockInChapter, RowSpan = 1});
+
+                if (IsLeftNeightbor(other))
+                {
+                    _row.SecondColumn.Add(new ChapterLayoutElem {Page = Rel, RowSpan = 1});
+                    return;
+                }
+                if (IsRightNeightbor(other))
+                {
+                    _row.FourthColumn.Add(new ChapterLayoutElem {Page = Rel, RowSpan = 1});
+                    return;
+                }
+                if (IsInBook(other))
+                {
+                    _row.FourthColumn.Add(new ChapterLayoutElem {Page = Rel, RowSpan = 1});
+                    _row.Inner = true;
+                    return;
+                }
+
+                _row.FirstColumn.Add(new ChapterLayoutElem {Page = other, RowSpan = 1});
+                _row.SecondColumn.Add(new ChapterLayoutElem {Page = Rel, RowSpan = 1});
+                return;
+            }
+
+            _row.ThirdColumn.Add(new ChapterLayoutElem {Page = Rel, RowSpan = 1});
+            if (!IsLeftNeightbor(Rel.RelationFirst) && !IsRightNeightbor(Rel.RelationFirst)
+                && !IsInBook(Rel.RelationFirst))
+                _row.SecondColumn.Add(new ChapterLayoutElem {Page = Rel.RelationFirst, RowSpan = 1});
+            if (!IsLeftNeightbor(Rel.RelationSecond) && !IsRightNeightbor(Rel.RelationSecond)
+                && !IsInBook(Rel.RelationSecond))
+                _row.FourthColumn.Add(new ChapterLayoutElem { Page = Rel.RelationSecond, RowSpan = 1 });
+        }
     }
 }
